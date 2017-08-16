536d3a0dacb74c326d77c66d408ac2f1b54c5c24
Sarah Abderemane <sarah.abderemane@gmail.com>
2017-08-16 22:43:12 +0200

    change visual page project-detail (css & js)

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,37 +1,11 @@
 $(document).ready(function(){
- //  $('.projects').slick({
- //  	slidesToShow: 3,
-	// slidesToScroll: 1,
-	// autoplay: true,
-	// autoplaySpeed: 8000,
-	// dots: true,
-	// responsive: [
- //    {
- //      breakpoint: 1024,
- //      settings: {
- //        slidesToShow: 3,
- //        slidesToScroll: 3,
- //        infinite: true,
- //        dots: true
- //      }
- //    },
- //    {
- //      breakpoint: 600,
- //      settings: {
- //        slidesToShow: 2,
- //        slidesToScroll: 2
- //      }
- //    },
- //    {
- //      breakpoint: 480,
- //      settings: {
- //        slidesToShow: 1,
- //        slidesToScroll: 1
- //      }
- //    }
- //    // You can unslick at a given breakpoint now by adding:
- //    // settings: "unslick"
- //    // instead of a settings object
- //  ]
- //  });
+  $('.slider').slick({
+  	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 4000,
+	dots: true,
+	arrows: false,
+	infinite: true,
+  });
 });
