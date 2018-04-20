6e7f2b77eb3f8007b12c0d14640d77e0a6b157e3
Sarah Abderemane <sarah.abderemane@gmail.com>
2018-04-20 11:25:29 +0200

    change links css / js

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -15,60 +15,84 @@ $(document).ready(function(){
  });
 
 /*********************
-	* works
+	* grid
 	*********************/
-	var $cont = document.querySelector('.cont');
-	var $elsArr = [].slice.call(document.querySelectorAll('.el'));
-	var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+	if($('.grid')) {
+		$('body').addClass('projects-on');
 
-	if($cont) {
-		setTimeout(function() {
-			$cont.classList.remove('s--inactive');
-		}, 200);
+		$('.nav-item').not('.nav-item.works').hide();
+		var grid = new Muuri('.grid', {
+			layout: {
+			    fillGaps: false,
+			    horizontal: true,
+			    alignRight: false,
+			    alignBottom: true,
+			    rounding: true
+			}
+		});
+	} else {
+		$('body').removeClass('projects-on');
+		$('.nav-item').not('.nav-item.home').hide();
 	}
 
-	$elsArr.forEach(function($el) {
-		$el.addEventListener('click', function() {
-			if (this.classList.contains('s--active')) return;
-			$cont.classList.add('s--el-active');
-			this.classList.add('s--active');
-			$name = $el.querySelector('.el__heading').innerHTML;
-			$name = $name.toLowerCase().replace(' ','-');
-			// change URL
-			window.history.replaceState(null, null, 'http://'+window.location.host+'/projects/'+name); 
-		});
-	});
 
-	$closeBtnsArr.forEach(function($btn) {
-		$btn.addEventListener('click', function(e) {
-			e.stopPropagation();
-			$cont.classList.remove('s--el-active');
-			document.querySelector('.el.s--active').classList.remove('s--active');
-			window.history.replaceState(null, null, 'http://'+window.location.host+'/projects/');
-		});
-
-	});
-
+});
 
 $(window).on('load', function(){
-	var $cont = document.querySelector('.cont');
-	var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+	// split title in letters with space
+	title = document.querySelector('.title-page');
+	if(title) {
+	    charming(title);
+	    //document.title.letters = Array.from(document.title.querySelectorAll('span'));
+    }
+
+	// var $cont = document.querySelector('.cont');
+	// var $elsArr = [].slice.call(document.querySelectorAll('.el'));
 
 	if(window.location.pathname != '/projects/') {
 		var $pathArray = window.location.pathname.split( '/' );
 		var $pathName = $pathArray[2];
 
-		$elsArr.forEach(function($el) {
-		// get all headings
-		$name = $el.querySelector('.el__heading').innerHTML;
-		$name = $name.toLowerCase().replace(' ','-');
-		// get the element html and show it
-		if($name == $pathName) {
-			$cont.classList.add('s--el-active');
-			$el.classList.add('s--active');
-		}
-	})
-
+		// $elsArr.forEach(function($el) {
+		// 	// get all headings
+		// 	$name = $el.querySelector('.el__heading').innerHTML;
+		// 	$name = $name.toLowerCase().replace(' ','-');
+		// 	// get the element html and show it
+		// 	if($name == $pathName) {
+		// 		$cont.classList.add('s--el-active');
+		// 		$el.classList.add('s--active');
+		// 	}
+		// })
 	}
 });  
 
+function getCookie(name) {
+	var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
