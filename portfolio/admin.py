fe1097f560a03d405d974cf71c581ec512e23d92
Sarah Abderemane <sarah.abderemane@gmail.com>
2019-02-03 19:22:32 +0100

    update translate

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,17 +2,44 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 from .models import Tag, Image, Project
 
 # Register your models here.
 admin.site.site_title = 'Portfolio Sarah Admin'
 admin.site.site_header = 'Portfolio Sarah Admin'
 
+class LanguageTabbedAdmin(TranslationAdmin):
+	class Media:
+		js = (
+			'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+			'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
+			'modeltranslation/js/tabbed_translation_fields.js',
+		)
+		css = {
+			'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+		}
 
-#admin.site.register(Tag)
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    fields = ('name','type','level')
+class TagAdmin(LanguageTabbedAdmin):
+	model = Tag
+	fields = ('name','type','level',)
+
+class TagInline(admin.TabularInline):
+	extra = 0
+	model = Project.tags.through
+	verbose_name = u"Tag"
+	verbose_name_plural = u"Tags"
+
+
+
+class ProjectAdmin(LanguageTabbedAdmin):
+	model = Project
+	inlines = (
+       TagInline,
+    )
+	exclude = ("tags", )
 
 admin.site.register(Image)
-admin.site.register(Project)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Project, ProjectAdmin)
+
--- portfolio/translation.py
+++ portfolio/translation.py
@@ -1,6 +1,10 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Project
+from .models import Project, Tag
+
+class TagTranslationOptions(TranslationOptions):
+    fields = ('name',)
+translator.register(Tag, TagTranslationOptions)
 
 class ProjectTranslationOptions(TranslationOptions):
-    fields = ('title','subtitle','description')
+    fields = ('title','subtitle','description',)
 translator.register(Project, ProjectTranslationOptions)
